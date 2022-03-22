9c2997bb5737cfeebcd9da8e2265dfd379ea5945
Vladislav2509 <vladislavdultev@mail.ru>
2022-03-22 13:13:57 +0300

    commit

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const firstName = 'Vlad'
-const lastName = 'Dultev'
-const age = 26
-const isProgrammer = true 
+// hw-2
 
-const asd = 'private'
-const $ = 'some value'
+// Task 1
+// const a = 25;
+// const b = 35;
+// const c = 45;
 
-const withNum5 = '5'
+// console.log(a + b + c);
 
-console.log('Имя человека: ' + firstName)
+// Task 2
+// let secend = 60;
+// let minut = 60;
+// let hour = 24;
+// let year = 365;
+
+// let myAgeInSeconds = (secend * minut * hour * year * 26);
+// console.log(myAgeInSeconds);
+
+//Task 3
+// let count = 42;
+// let userName = '42';
+
+// count = String(count);
+// userName = Number(userName);
+// console.log(count, userName);
+// console.log(String(count), Number(userName));
+
+//Task 4
+// let a = 1;
+// let b = 2;
+// let c = 'белых медведей';
+
+// a = String(a);
+// b = String(b);
+// console.log( a + b, c);
+
+// console.log(String(a) + String(b), c);
+
+// let vlad = (`${a+b} ${c} я эту задачу все-таки решил!`);
+// console.log(vlad);  
+
+
+
