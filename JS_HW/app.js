8397bfd84b47080e9143db7d5f21c224cb007031
Vladislav2509 <vladislavdultev@mail.ru>
2022-03-24 17:22:54 +0300

    hw2

---
+++ JS_HW/app.js
@@ -0,0 +1,115 @@
+// hw-2
+
+// Task 1
+
+// const a = 25;
+// const b = 35;
+// const c = 45;
+
+// console.log(a + b + c);
+
+
+// Task 2
+
+// let secend = 60;
+// let minut = 60;
+// let hour = 24;
+// let year = 365;
+
+// let myAgeInSeconds = (secend * minut * hour * year * 26);
+// console.log(myAgeInSeconds);
+
+
+//Task 3
+
+// let count = 42;
+// let userName = '42';
+
+// count = String(count);
+// userName = Number(userName);
+// console.log(count, userName);
+// console.log(String(count), Number(userName));
+
+
+//Task 4
+
+// let a = 1;
+// let b = 2;
+// let c = 'белых медведей';
+
+// a = String(a);
+// b = String(b);
+// console.log( a + b, c);
+
+// console.log(String(a) + String(b), c);
+
+// let vlad = (`${a+b} ${c} я эту задачу все-таки решил!)`);
+// console.log(vlad); 
+
+
+//Task 5
+
+// const a = 'доступ';
+// const b = 'морпех';
+// const c = 'наледь';
+// const d = 'попрек';
+// const e = 'рубило';
+
+// const lengthWords = (a.length + b.length + c.length + d.length + e.length);
+
+// console.log(`Длинна всех слов из списка: - ${lengthWords}`);
+
+
+//Task 6
+
+// let typeNumber = 5;
+// let typeString = "Влад";
+// let typeBoolean = false;
+
+// console.log(`Переменная: typeNumber имеет тип: - ${typeof typeNumber}`);
+// console.log(`Переменная: typeString имеет тип: - ${typeof typeString}`);
+// console.log(`Переменная: typeBoolean имеет тип: - ${typeof typeBoolean}`);
+
+
+//Task 7
+
+// let userName = prompt ('Введите имя пользователя');
+// let age = prompt ('Введите возраст пользователя')
+
+// console.log(`Ваше имя: - ${userName}`);
+// console.log( `Ваш возраст: - ${age}`);
+
+
+
+// ADVANCED level
+
+// Task 1
+
+// let a = 4;
+// a = 3;
+// let b = 3;
+// b = 4;
+
+// console.log(`в переменной а: - ${a}`);
+// console.log(`в переменной а: - ${b}`);
+
+
+// Task 2
+
+// let codeWord1 = "обернись";
+// let codeWord2 = "неужели";
+// let codeWord3 = "огурцы";
+// let codeWord4 = "липкие";
+// let codeWord5 = "?!";
+
+// let cipher = (codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]);
+
+// console.log(cipher);
+
+
+
+
+
+
+
+
